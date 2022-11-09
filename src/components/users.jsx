af37ab5b3ce5a2cebb17b57a6d07f513db49960a
kuRajj <swift.91@mail.ru>
2022-11-10 02:00:56 +0300

    some changes

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,6 +9,24 @@ const Users = () => {
     return classes;
   };
 
+  const renderTable = () => {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>{renderUsers()}</tbody>
+      </table>
+    );
+  };
+
   const renderUsers = () => {
     return users.map((user) => (
       <tr key={user._id}>
@@ -74,23 +92,6 @@ const Users = () => {
         </>
       );
     }
-  };
-  const renderTable = () => {
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>{renderUsers()}</tbody>
-      </table>
-    );
   };
 
   return renderMessage();
