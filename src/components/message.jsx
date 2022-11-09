fecc6008f0cf56f6971a2db42204c9a6303c8362
kuRajj <swift.91@mail.ru>
2022-11-10 01:49:23 +0300

    delete component

--- src/components/message.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import api from "../api";
-import Users from "./users";
-
-export const Message = () => {
-  const [mess, setMesss] = useState(api.users.fetchAll());
-
-  return <h1>{Users.users.length} человек</h1>;
-};
