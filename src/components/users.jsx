b4d80e3e52d82c1701557a7fbc3259a20bf6c93e
kuRajj <swift.91@mail.ru>
2022-11-10 23:04:37 +0300

    change render table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,52 +9,33 @@ const Users = () => {
     return classes;
   };
 
-  const renderTable = () => {
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>{renderUsers()}</tbody>
-      </table>
-    );
-  };
-
-  const renderUsers = () => {
-    return users.map((user) => (
-      <tr key={user._id}>
-        <th scope="row">{user.name}</th>
-        <th scope="row">
-          {user.qualities.map((qualitie) => (
-            <span className={setBageClasses(qualitie)} key={qualitie._id}>
-              {qualitie.name}
-            </span>
-          ))}
-        </th>
-        <th scope="row">{user.profession.name}</th>
-        <th scope="row">{user.completedMeetings}</th>
-        <th scope="row">{user.rate}</th>
-        <th>
-          <button
-            className="btn btn-danger btn-sm m-1"
-            onClick={() => handleDeleteUser(user)}
-          >
-            delete
-          </button>
-        </th>
-      </tr>
-    ));
-  };
-
-  const handleDeleteUser = (id) => {
+  const handleDelet = (id) => {
     setUsers((prevState) => prevState.filter((user) => user !== id));
+  };
+
+  const renderPhrase = (number) => {
+    if (number < 1) {
+      return (
+        <span
+          className="howManyPeopleHaveFunWithMe"
+          style={{ background: "rgb(202, 33, 4)" }}
+        >
+          Ты никому не нужен Т_Т
+        </span>
+      );
+    } else {
+      return (
+        <span className="howManyPeopleHaveFunWithMe">
+          {users.length}{" "}
+          {howManyPeopleHaveFunWithMe(number, [
+            "человек",
+            "человека",
+            "человек",
+          ])}{" "}
+          тусанет с тобой сегодня
+        </span>
+      );
+    }
   };
 
   const howManyPeopleHaveFunWithMe = (value, words) => {
@@ -66,37 +47,49 @@ const Users = () => {
     return words[2];
   };
 
-  const renderBadMessage = (text) => {
-    return (
-      <span
-        className="howManyPeopleHaveFunWithMe"
-        style={{ background: "rgb(202, 33, 4)" }}
-      >
-        {text}
-      </span>
-    );
-  };
-
-  const renderGoodMessage = (text) => {
-    return (
-      <>
-        <span className="howManyPeopleHaveFunWithMe">
-          {users.length}{" "}
-          {howManyPeopleHaveFunWithMe(users.length, [
-            "человек",
-            "человека",
-            "человек",
-          ])}{" "}
-          {text}
-        </span>
-        {renderTable()}
-      </>
-    );
-  };
-
-  return users.length < 1
-    ? renderBadMessage("Ты никому не нужен Т_Т")
-    : renderGoodMessage("тусанет с тобой сегодня");
+  return users.length >= 1 ? (
+    <>
+      {renderPhrase(users.length)}
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <th scope="row">{user.name}</th>
+              <th scope="row">
+                {user.qualities.map((qualitie) => (
+                  <span className={setBageClasses(qualitie)} key={qualitie._id}>
+                    {qualitie.name}
+                  </span>
+                ))}
+              </th>
+              <th scope="row">{user.profession.name}</th>
+              <th scope="row">{user.completedMeetings}</th>
+              <th scope="row">{user.rate}</th>
+              <th>
+                <button
+                  className="btn btn-danger btn-sm m-1"
+                  onClick={() => handleDelet(user)}
+                >
+                  delete
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  ) : (
+    renderPhrase(users.length)
+  );
 };
-
 export default Users;
